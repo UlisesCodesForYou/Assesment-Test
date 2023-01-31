1951a76387ddddaedaef6dea48c12cc6d35ff8c5
Ulises Orozco <ulisesorozco6@gmail.com>
2023-01-31 15:22:13 -0800

    Added top bar to the third section along with text

--- components/ThirdSection/ThirdSection.tsx
+++ components/ThirdSection/ThirdSection.tsx
@@ -5,14 +5,18 @@ export const ThirdSection = () => {
   const { classes } = useStyles();
 
   return (
-    <Card style={{ backgroundImage: 'url(Snowy-Mountain2.png)' }} className={classes.card}>
-      <div className={classes.rectangle}>
-        <Image width="auto" height="auto" src="Rectangle-Image.png" />
-      </div>
-      <div className={classes.schedule}>
-        <Image src="Schedule-Image.png" width={120} height="auto" />
-      </div>
-      <Image className={classes.dates} src="Mountain-Dates-2.png" width="auto" height="auto" />
-    </Card>
+    <>
+      <Image src="Rectangle 20.png" width="auto" height="auto" />
+      <Image src="Mountain 1.png" width="auto" height="auto" />
+      <Card style={{ backgroundImage: 'url(Snowy-Mountain2.png)' }} className={classes.card}>
+        <div className={classes.rectangle}>
+          <Image width="auto" height="auto" src="Rectangle-Image.png" />
+        </div>
+        <div className={classes.schedule}>
+          <Image src="Schedule-Image.png" width={120} height="auto" />
+        </div>
+        <Image className={classes.dates} src="Mountain-Dates-2.png" width="auto" height="auto" />
+      </Card>
+    </>
   );
 };
